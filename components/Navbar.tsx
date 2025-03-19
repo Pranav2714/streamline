5846c57f3d7ade9fd0103c17532bfc90c40bb415
Pranav Mahamunkar <pranav.mahamunkar11@gmail.com>
2025-03-20 01:50:02 +0530

    adding MobileNavbar and navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex-between fixed  z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src={"/icons/duo-logo.svg"}
+          alt="StreamLine"
+          width="32"
+          height="32"
+        />
+        <p className="text-[26px] font-extrabold max-sm:hidden text-white">
+          StreamLine
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        {/* clerk management */}
+        <MobileNav />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
